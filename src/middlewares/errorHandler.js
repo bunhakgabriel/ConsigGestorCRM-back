b27ef35fc5d9828dc576b95171fef8cb5a0b0ad1
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-03-24 20:27:55 -0300

    adiciona validacao para violação de chave unica no banco

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,13 +1,24 @@
 import ApiResponse from "../api/ApiResponse.js";
 import { AppError } from "../utils/AppError.js";
+import { sqlConstraintMessages } from "../utils/sqlErrors.js";
 
 export function errorHandler(err, _req, res, _next) {
+  // Erros ja tratados
   if (err instanceof AppError) {
     return res
       .status(err.statusCode)
       .json(new ApiResponse(false, null, err.message));
   }
 
+  // Erro de violação de unique do PostgreSQL
+  if (err.code === "23505") {
+    const message =
+      sqlConstraintMessages[err.constraint] ||
+      "Violação de chave única no banco de dados.";
+    return res.status(400).json(new ApiResponse(false, null, message));
+  }
+
+  // Erro genérico
   return res
     .status(500)
     .json(new ApiResponse(false, null, "Erro interno do servidor"));
---
+++ src/utils/sqlErrors.js
@@ -0,0 +1,4 @@
+export const sqlConstraintMessages = {
+  clientes_cpf_key: "Já existe um cliente com este CPF.",
+  clientes_rg_key: "Já existe um cliente com este RG.",
+};
