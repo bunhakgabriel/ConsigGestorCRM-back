421ca4e9db64bac676f31cfdefb015c6eb4ad981
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-03-24 00:50:42 -0300

    corrige erro de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import route from "./routes/index.js"
 import { errorHandler } from "../middlewares/errorHandler.js"
+import cors from 'cors';
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 route(app)
