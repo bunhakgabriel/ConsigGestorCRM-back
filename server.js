61404ab27e6b8040b61477ce19358e76713f7914
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-06-17 17:12:45 -0300

    adiciona acesso externo para requisições

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`)
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor escutando em http://0.0.0.0:${port}`)
 })
